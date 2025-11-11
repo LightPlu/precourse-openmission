4d97f41f608ad8cba549304badae34c094473877
배재훈 <lange50300@gmail.com>
2025-11-12 00:40:50 +0900

    refactor: LottoResult Entity 수정
    - 기존 Map을 활용했던걸 PrizeDetail VO를 활용하도록 수정한다.
    - 기존 등수별 카운트 갯수를 반환하던 메서드를 삭제하고, 로또 게임 1장의
    상금을 반환하는 메서드를 추가한다.

--- src/main/java/lotto/domain/entity/LottoResult.java
+++ src/main/java/lotto/domain/entity/LottoResult.java
@@ -1,27 +1,23 @@
 package lotto.domain.entity;
 
 import java.util.List;
-import java.util.Map;
-import lotto.domain.vo.Rank;
+import lotto.domain.vo.PrizeDetail;
 
 public class LottoResult {
-    private final Map<Rank, Integer> results;
+    private final List<PrizeDetail> results;
 
-    public LottoResult(Map<Rank, Integer> results) {
-        this.results = results;
+    public LottoResult(List<PrizeDetail> results) {
+        this.results = List.copyOf(results);
     }
 
-    public Map<Rank, Integer> getResults() {
+    public List<PrizeDetail> getResults() {
         return results;
     }
 
-    public List<Integer> getResultValuesOrdered() {
-        return List.of(
-                results.getOrDefault(Rank.FIFTH, 0),    // 5등 (3개 일치)
-                results.getOrDefault(Rank.FOURTH, 0),   // 4등 (4개 일치)
-                results.getOrDefault(Rank.THIRD, 0),    // 3등 (5개 일치)
-                results.getOrDefault(Rank.SECOND, 0),   // 2등 (5개+보너스)
-                results.getOrDefault(Rank.FIRST, 0)     // 1등 (6개 일치)
-        );
+    public double calculateAllPrizes() {
+        return results.stream()
+                .mapToDouble(PrizeDetail::getTotalPrize)
+                .sum();
     }
+
 }
