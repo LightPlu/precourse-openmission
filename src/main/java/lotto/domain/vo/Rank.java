b7206a142d8f7b832fab5849c4bec78751ef5bb5
배재훈 <lange50300@gmail.com>
2025-11-12 20:34:08 +0900

    refactor: valueOf 메서드 리팩토링
    - 기존 if로 분기하던 조건문을 Stream API를 활용하도록 수정합니다.

--- src/main/java/lotto/domain/vo/Rank.java
+++ src/main/java/lotto/domain/vo/Rank.java
@@ -1,5 +1,7 @@
 package lotto.domain.vo;
 
+import java.util.Arrays;
+
 public enum Rank {
     FIRST(6, false, 2_000_000_000),
     SECOND(5, true, 30_000_000),
@@ -19,23 +21,15 @@ public enum Rank {
     }
 
     public static Rank valueOf(int matchCount, boolean bonusMatch) {
-        if (matchCount == 6) {
-            return FIRST;
-        }
-        if (matchCount == 5 && bonusMatch) {
-            return SECOND;
-        }
-        if (matchCount == 5) {
-            return THIRD;
-        }
-        if (matchCount == 4) {
-            return FOURTH;
-        }
-        if (matchCount == 3) {
-            return FIFTH;
-        }
-        return MISS;
+        return Arrays.stream(Rank.values())
+                .filter(rank ->
+                        rank.matchCount == matchCount &&
+                                (!rank.bonusMatch || (rank.bonusMatch == bonusMatch))
+                )
+                .findFirst()
+                .orElse(MISS);
     }
+
 
     public int getPrize() {
         return prize;
