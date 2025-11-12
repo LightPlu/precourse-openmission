2e1d6dc7bfbdbd1883707bd28558a9e8011d0dc1
배재훈 <lange50300@gmail.com>
2025-11-12 18:14:01 +0900

    feat: Cash VO 멤버변수 및 검증 로직 추가
    - 금액에 대한 로또 구매 횟수를 관리하는 멤버변수를 추가합니다.
    - 한번 구매할 때 최대 10만원의 금액을 제한하는 검증로직을 추가합니다.

--- src/main/java/lotto/domain/vo/Cash.java
+++ src/main/java/lotto/domain/vo/Cash.java
@@ -1,17 +1,25 @@
 package lotto.domain.vo;
 
+import static lotto.domain.NumberConstants.LOTTO_GAME_MAX_PURCHASE;
+import static lotto.domain.NumberConstants.LOTTO_PURCHASE_MAX_MONEY;
 import static lotto.exceptions.ErrorMessage.CASH_IS_NOT_DIVISIBLE;
 import static lotto.exceptions.ErrorMessage.CASH_IS_NOT_POSITIVE;
 import static lotto.domain.NumberConstants.LOTTO_PRICE;
 import static lotto.domain.NumberConstants.ZERO;
+import static lotto.exceptions.ErrorMessage.CASH_RANGE_OUT;
 
 public class Cash {
     private final int money;
+    private final int fullGameCount;
+    private final int remainingGameCount;
 
     public Cash(int money) {
         validateMoneyPositive(money);
+        validateMoneyRange(money);
         validateMoneyDivisible(money);
         this.money = money;
+        this.fullGameCount = calculateFullGameCount(money);
+        this.remainingGameCount = calculateRemainingGameCount(money);
     }
 
     private void validateMoneyDivisible(int money) {
@@ -26,7 +34,30 @@ public class Cash {
         }
     }
 
+    private void validateMoneyRange(int money) {
+        if (money > LOTTO_PURCHASE_MAX_MONEY.getValue()) {
+            throw new IllegalArgumentException(CASH_RANGE_OUT.getMessage());
+        }
+    }
+
+    private int calculateFullGameCount(int money) {
+        return money / LOTTO_GAME_MAX_PURCHASE.getValue();
+    }
+
+    private int calculateRemainingGameCount(int money) {
+        int eachMoney = money % LOTTO_GAME_MAX_PURCHASE.getValue();
+        return eachMoney / LOTTO_PRICE.getValue();
+    }
+
     public int getMoney() {
         return money;
+    }
+
+    public int getFullGameCount() {
+        return fullGameCount;
+    }
+
+    public int getRemainingGameCount() {
+        return remainingGameCount;
     }
 }
