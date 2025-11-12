ac281bb2f0d6647beab1425cde5bec59c1da081b
배재훈 <lange50300@gmail.com>
2025-11-12 15:05:05 +0900

    feat: LottoResult Entity 멤버변수 추가
    - 각 회차를 저장할 수 있는 round 멤버 변수를 추가합니다.

--- src/main/java/lotto/domain/entity/LottoResult.java
+++ src/main/java/lotto/domain/entity/LottoResult.java
@@ -4,9 +4,11 @@ import java.util.List;
 import lotto.domain.vo.PrizeDetail;
 
 public class LottoResult {
+    private final int round;
     private final List<PrizeDetail> results;
 
-    public LottoResult(List<PrizeDetail> results) {
+    public LottoResult(int round, List<PrizeDetail> results) {
+        this.round = round;
         this.results = List.copyOf(results);
     }
 
