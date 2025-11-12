0b29e2e0db5096908dfd2c556bcda44ea32060b4
배재훈 <lange50300@gmail.com>
2025-11-12 20:36:43 +0900

    feat: NumberConstants 상수 추가
    - 한 게임당 최대 구매 개수인 5개를 표현하는 LOTTO_GAME_MAX_PURCHASE 상수를 추가합니다.
    - 로또 한번 구매 최대 금액인 100000원을 표현하는 LOTTO_PURCHASE_MAX_MONEY 상수를 추가합니다.

--- src/main/java/lotto/domain/NumberConstants.java
+++ src/main/java/lotto/domain/NumberConstants.java
@@ -3,7 +3,9 @@ package lotto.domain;
 public enum NumberConstants {
 
     LOTTO_PRICE(1000),
-    LOTTO_SIZE(6),
+    LOTTO_NUMBER_SIZE(6),
+    LOTTO_GAME_MAX_PURCHASE(5),
+    LOTTO_PURCHASE_MAX_MONEY(100000),
     LOTTO_START_NUMBER(1),
     LOTTO_END_NUMBER(45),
     ZERO(0),
