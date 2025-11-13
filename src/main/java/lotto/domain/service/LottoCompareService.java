fbb31ada13ca31d87e37633dca94ef5ecd0f58a7
배재훈 <lange50300@gmail.com>
2025-11-13 23:40:52 +0900

    feat: LottoCompareService 구현
    - 각 로또 티켓별 당첨번호와 비교 후 결과값을 반환하는 compareTicket
    메서드를 구현합니다.

--- src/main/java/lotto/domain/service/LottoCompareService.java
+++ src/main/java/lotto/domain/service/LottoCompareService.java
@@ -1,14 +1,42 @@
 package lotto.domain.service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import lotto.domain.entity.LottoTicket;
 import lotto.domain.vo.Lotto;
 import lotto.domain.vo.CountResult;
 import lotto.domain.entity.WinningLottoNumbers;
 import lotto.domain.vo.LottoNumber;
+import lotto.domain.vo.PrizeDetail;
+import lotto.domain.vo.Rank;
 
 public class LottoCompareService {
 
-    public CountResult compareNumber(Lotto lotto, WinningLottoNumbers winningLottoNumbers) {
+    public List<PrizeDetail> compareTicket(LottoTicket lottoTicket, WinningLottoNumbers winningLottoNumbers) {
+        Map<Rank, Integer> rankResults = countingResults(lottoTicket,  winningLottoNumbers);
+
+        return rankResults.entrySet().stream()
+                .map(entry -> PrizeDetail.of(entry.getKey(), entry.getValue()))
+                .toList();
+    }
+
+    private Map<Rank, Integer> countingResults(LottoTicket lottoTicket, WinningLottoNumbers winningLottoNumbers) {
+        Map<Rank, Integer> rankResults = new HashMap<>();
+        for (Rank rank : Rank.values()) {
+            rankResults.put(rank, 0);
+        }
+        List<Lotto> lottos = lottoTicket.getLottos();
+        lottos.forEach(lotto -> {
+            CountResult result = compareNumber(lotto, winningLottoNumbers);
+            Rank rank = Rank.valueOf(result.getNumberMatchCount(), result.getBonusNumberMatchCount());
+
+            rankResults.put(rank, rankResults.getOrDefault(rank, 0) + 1);
+        });
+        return rankResults;
+    }
+
+    private CountResult compareNumber(Lotto lotto, WinningLottoNumbers winningLottoNumbers) {
         List<LottoNumber> lottoNumbers = lotto.getNumbers();
         List<LottoNumber> winningNumbers = winningLottoNumbers.getWinningNumbers();
         LottoNumber bonusNumber = winningLottoNumbers.getBonusNumber();
@@ -23,4 +51,6 @@ public class LottoCompareService {
 
         return new CountResult(numberMatchCount, false);
     }
+
+
 }
