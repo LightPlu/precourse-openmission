7191b2f58896ab0ff930bcb3ea1b462b4dbdc87e
배재훈 <lange50300@gmail.com>
2025-11-13 19:54:41 +0900

    feat: LottoPurchaseService 클래스 구현
    - 결제된 금액에 맞춰 로또 Ticket을 구매하는 로직을 구현합니다.

--- src/main/java/lotto/domain/service/LottoPurchaseService.java
+++ src/main/java/lotto/domain/service/LottoPurchaseService.java
@@ -1,30 +1,42 @@
 package lotto.domain.service;
 
-import static lotto.utils.NumberConstants.LOTTO_PRICE;
+import static lotto.domain.NumberConstants.LOTTO_GAME_MAX_PURCHASE;
+import static lotto.domain.NumberConstants.ZERO;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.IntStream;
-import lotto.domain.entity.Lotto;
+import lotto.domain.entity.LottoTicket;
 import lotto.domain.vo.Cash;
-import lotto.utils.RandomNumberGenerator;
+import lotto.domain.vo.Lotto;
 
 public class LottoPurchaseService {
 
-    private final RandomNumberGenerator randomNumberGenerator;
+    private final LottoNumberGenerator lottoNumberGenerator;
 
-    public LottoPurchaseService(RandomNumberGenerator randomNumberGenerator) {
-        this.randomNumberGenerator = randomNumberGenerator;
+    public LottoPurchaseService(LottoNumberGenerator randomNumberGenerator) {
+        this.lottoNumberGenerator = randomNumberGenerator;
     }
 
-    public List<Lotto> purchase(Cash cash) {
-        int count = calculateCount(cash);
+    public List<LottoTicket> purchase(Cash cash) {
+        List<LottoTicket> lottoTickets = new ArrayList<>();
 
-        return IntStream.range(0, count)
-                .mapToObj(i -> new Lotto(randomNumberGenerator.numberCreates()))
+        IntStream.range(ZERO.getValue(), cash.getFullGameCount())
+                .mapToObj(i -> createLottoTicket(LOTTO_GAME_MAX_PURCHASE.getValue()))
+                .forEach(lottoTickets::add);
+
+        if (cash.getRemainingGameCount() > ZERO.getValue()) {
+            lottoTickets.add(createLottoTicket(cash.getRemainingGameCount()));
+        }
+
+        return lottoTickets;
+    }
+
+    private LottoTicket createLottoTicket(int gameCount) {
+        List<Lotto> lottos = IntStream.range(ZERO.getValue(), gameCount)
+                .mapToObj(i -> new Lotto(lottoNumberGenerator.createNumbers()))
                 .toList();
+        return new LottoTicket(lottos);
     }
 
-    private int calculateCount(Cash cash) {
-        return cash.getMoney() / LOTTO_PRICE.getValue();
-    }
 }
