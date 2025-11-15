15ca5b356f457c4b03c86a434f3970695d3f0cfa
배재훈 <lange50300@gmail.com>
2025-11-15 21:01:36 +0900

    refactor: 클래스명 수정
    - Lotto domain에 관련된 내용이 없으므로 클래스명에서 Lotto를 제외시킵니다.

--- src/main/java/lotto/infrastructure/RandomNumberGeneratorImpl.java
+++ src/main/java/lotto/infrastructure/RandomNumberGeneratorImpl.java
@@ -4,7 +4,7 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 import lotto.domain.service.RandomNumberGenerator;
 
-public class LottoRandomNumberGenerator implements RandomNumberGenerator {
+public class RandomNumberGeneratorImpl implements RandomNumberGenerator {
 
     @Override
     public List<Integer> createNumbers(int min, int max, int count) {
