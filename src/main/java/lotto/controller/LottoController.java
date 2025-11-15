f7c71e3d9869975416a880d61fef47af4c255f2e
배재훈 <lange50300@gmail.com>
2025-11-15 21:12:10 +0900

    feat: LottoController 구현
    - 전체적인 게임 로직을 관리하는 Controller를 구현합니다.

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,77 +1,105 @@
 package lotto.controller;
 
 import java.util.List;
-import lotto.domain.vo.Lotto;
-import lotto.domain.entity.WinningLottoNumbers;
-import lotto.service.LottoServiceImpl;
+import lotto.domain.entity.RoundResult;
+import lotto.service.RoundApplicationService;
+import lotto.service.RoundResultApplicationService;
+import lotto.service.TicketApplicationService;
+import lotto.service.WinningLottoApplicationService;
 import lotto.view.UserInputView;
 import lotto.view.UserOutputView;
 
 public class LottoController {
 
-    UserOutputView userOutputView = new UserOutputView();
-    UserInputView userInputView = new UserInputView();
-    LottoServiceImpl lottoService = new LottoServiceImpl();
+    private final RoundApplicationService roundService;
+    private final TicketApplicationService ticketService;
+    private final WinningLottoApplicationService winningService;
+    private final RoundResultApplicationService resultService;
+    private final UserInputView userInputView = new UserInputView();
+    private final UserOutputView userOutputView = new UserOutputView();
+
+
+    public LottoController(
+            RoundApplicationService roundService,
+            TicketApplicationService ticketService,
+            WinningLottoApplicationService winningService,
+            RoundResultApplicationService resultService
+    ) {
+        this.roundService = roundService;
+        this.ticketService = ticketService;
+        this.winningService = winningService;
+        this.resultService = resultService;
+    }
 
     public void run() {
-        int price = getPriceWithRetry();
-        int count = buyLottoWithRetry(price);
-
-        userOutputView.printLottoCountMessage(count);
-        String lottoHistory = lottoService.printLottoPurchaseHistory();
-        userOutputView.printLottoHistory(lottoHistory);
-
-        List<Integer> winningNumbers = getWinningNumbersWithRetry();
-
-        int bonusNumber = getBonusNumberWithRetry(winningNumbers);
-
-        double earningRate = lottoService.compareLottoAndAggregate(price, winningNumbers, bonusNumber);
-        List<Integer> winningStats = lottoService.printLottoResultStatistics(winningNumbers, bonusNumber);
-        userOutputView.printTotalStatics(winningStats, earningRate);
-    }
-
-    private int getPriceWithRetry() {
         while (true) {
-            try {
-                return userInputView.printPriceMessage();
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
+            userOutputView.printMenu();
+            int choice = userInputView.printChoiceMenuMessage();
+
+            switch (choice) {
+                case 1 -> startNewRound();
+                case 2 -> buyTickets();
+                case 3 -> registerWinningNumbers();
+                case 4 -> closeRound();
+                case 5 -> showRoundResult();
+                case 0 -> {
+                    System.out.println("프로그램 종료");
+                    return;
+                }
+                default -> System.out.println("[ERROR] 잘못된 입력");
             }
         }
     }
 
-    private int buyLottoWithRetry(int price) {
-        while (true) {
-            try {
-                return lottoService.buyLottoAndSave(price);
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
-                price = getPriceWithRetry();
-            }
+    private void startNewRound() {
+        roundService.startNewRound();
+    }
+
+    private void buyTickets() {
+        int money = userInputView.printPriceMessage();
+
+        try {
+            ticketService.buyTicketsAndSave(money);
+        } catch (Exception e) {
+            System.out.println("[ERROR] " + e.getMessage());
         }
     }
 
-    private List<Integer> getWinningNumbersWithRetry() {
-        while (true) {
+    private void registerWinningNumbers() {
+        while(true) {
             try {
                 List<Integer> winningNumbers = userInputView.printWinningNumbersMessage();
-                new Lotto(winningNumbers);
-                return winningNumbers;
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
+                int bonus = userInputView.printBonusNumbersMessage();
+
+                winningService.saveWinningNumbers(winningNumbers, bonus);
+                break;
+            } catch (Exception e) {
+                System.out.println("[ERROR] " + e.getMessage());
             }
+        }
+
+    }
+
+    private void closeRound() {
+
+        try {
+            roundService.closeRoundAndStartNextRound();
+        } catch (Exception e) {
+            System.out.println("[ERROR] " + e.getMessage());
         }
     }
 
-    private int getBonusNumberWithRetry(List<Integer> winningNumbers) {
-        while (true) {
-            try {
-                int bonusNumber = userInputView.printBonusNumbersMessage();
-                new WinningLottoNumbers(winningNumbers, bonusNumber);
-                return bonusNumber;
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
-            }
+    private void showRoundResult() {
+        List<Integer> roundNumbers = roundService.findAllRoundNumbers();
+        userOutputView.printExistRound(roundNumbers);
+        int round = userInputView.printChoiceRoundNumberMessage();
+
+        try {
+            RoundResult rr = resultService.getRoundResult(round);
+            userOutputView.printRoundResult(rr);
+        } catch (Exception e) {
+            System.out.println("[ERROR] " + e.getMessage());
         }
     }
 }
+
