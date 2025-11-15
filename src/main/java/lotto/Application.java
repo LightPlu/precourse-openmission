78988b5d91592095d4b26cbf2483281e6ac5e7b7
배재훈 <lange50300@gmail.com>
2025-11-15 21:13:47 +0900

    feat: Application main 구현
    - Config 및 Controller 객체를 생성하고 프로그램을 시작합니다.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,15 +1,14 @@
 package lotto;
 
+import lotto.config.ApplicationConfig;
 import lotto.controller.LottoController;
 
 public class Application {
     public static void main(String[] args) {
 
-        LottoController controller = new LottoController();
-        try {
-            controller.run();
-        } catch (Exception e) {
-            return;
-        }
+        ApplicationConfig applicationConfig = new ApplicationConfig();
+        LottoController lottoController = applicationConfig.lottoController();
+
+        lottoController.run();
     }
 }
