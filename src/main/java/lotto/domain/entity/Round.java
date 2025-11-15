6243539272e3cd84b5aa9abd269704d78771e66e
배재훈 <lange50300@gmail.com>
2025-11-15 22:20:11 +0900

    refactor: 사용하지 않는 멤버변수 및 메서드 삭제

--- src/main/java/lotto/domain/entity/Round.java
+++ src/main/java/lotto/domain/entity/Round.java
@@ -7,9 +7,6 @@ public class Round {
     private final int id;
     private final int roundNumber;
 
-    private WinningLottoNumbers winningLottoNumbers;
-    private RoundResult roundResult;
-
     public Round(int id, int roundNumber) {
         this.id = id;
         this.roundNumber = roundNumber;
@@ -25,28 +22,6 @@ public class Round {
 
     public int getRoundNumber() {
         return roundNumber;
-    }
-
-    public WinningLottoNumbers getWinningLottoNumbers() {
-        return winningLottoNumbers;
-    }
-
-    public void registerWinningNumbers(WinningLottoNumbers winningLottoNumbers) {
-        if (this.winningLottoNumbers != null) {
-            throw new IllegalStateException("이미 당첨 번호가 등록된 회차입니다.");
-        }
-        this.winningLottoNumbers = winningLottoNumbers;
-    }
-
-    public RoundResult getRoundResult() {
-        return roundResult;
-    }
-
-    public void registerResult(RoundResult result) {
-        if (this.roundResult != null) {
-            throw new IllegalStateException("이미 결과가 등록된 회차입니다.");
-        }
-        this.roundResult = result;
     }
 
     @Override
