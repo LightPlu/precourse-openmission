05f53585efd7c94da8515dd2549be76534b0d837
배재훈 <lange50300@gmail.com>
2025-11-17 19:35:13 +0900

    feat: override 메서드 추가
    - 테스트를 위하여 멤버변수의 final을 삭제하고 override 메서드를 추가합니다.

--- src/main/java/lotto/infrastructure/db/DBConnectionManager.java
+++ src/main/java/lotto/infrastructure/db/DBConnectionManager.java
@@ -6,13 +6,17 @@ import java.sql.SQLException;
 
 public class DBConnectionManager {
 
-    private static final String URL = "jdbc:postgresql://127.0.0.1:5432/postgres";
-    private static final String USER = "lightplu";
-    private static final String PASSWORD = "1234";
-
-    private DBConnectionManager() { }
+    private static String url = "jdbc:postgresql://127.0.0.1:5432/postgres";
+    private static String user = "lightplu";
+    private static String password = "1234";
 
     public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USER, PASSWORD);
+        return DriverManager.getConnection(url, user, password);
+    }
+
+    public static void override(String newUrl, String newUsername, String newPassword) {
+        url = newUrl;
+        user = newUsername;
+        password = newPassword;
     }
 }
