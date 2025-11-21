73932a22d56f70b56c51fc0247e5ace0301fff42
배재훈 <lange50300@gmail.com>
2025-11-21 18:53:08 +0900

    refactor: 변수 타입 변경
    - 변수를 애그리거트 루트로 변경하여 관리합니다.

--- src/main/java/lotto/application/RoundResultApplicationServiceImpl.java
+++ src/main/java/lotto/application/RoundResultApplicationServiceImpl.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Map;
 import lotto.domain.lottoTicket.entity.LottoTicket;
 import lotto.domain.lottoTicket.repository.LottoTicketRepository;
+import lotto.domain.round.entity.Round;
 import lotto.domain.round.vo.RoundResult;
 import lotto.domain.round.repository.RoundRepository;
 
@@ -22,22 +23,26 @@ public class RoundResultApplicationServiceImpl implements RoundResultApplication
     }
 
     @Override
-    public Map<String, Integer> getRoundResult(int roundNumber) {
-        RoundResult result = roundRepository.findRoundResultByRoundId(roundNumber)
+    public Map<String, Integer> getRoundResult(int roundId) {
+        Round round = roundRepository.findByRoundId(roundId)
                 .orElseThrow(() -> new RuntimeException(NOT_FINISHED_ROUND.getMessage()));
+
+        RoundResult result = round.getRoundResult();
         return result.toMap();
     }
 
     @Override
-    public Map<String, Long> calculateWinningPrize(int roundNumber) {
-        RoundResult result = roundRepository.findRoundResultByRoundId(roundNumber)
+    public Map<String, Long> calculateWinningPrize(int roundId) {
+        Round round = roundRepository.findByRoundId(roundId)
                 .orElseThrow(() -> new RuntimeException(NOT_FINISHED_ROUND.getMessage()));
+
+        RoundResult result = round.getRoundResult();
         return result.calculateWinningPrize();
     }
 
     @Override
-    public long calculateLottoSize(int roundNumber) {
-        List<LottoTicket> tickets = lottoTicketRepository.findTicketsByRoundId(roundNumber);
+    public long calculateLottoSize(int roundId) {
+        List<LottoTicket> tickets = lottoTicketRepository.findTicketsByRoundId(roundId);
         return tickets.stream()
                 .mapToLong(LottoTicket::calculateLottoSize)
                 .sum();
